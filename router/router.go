c0492077b46c729a7754e4468f25758fc32e619f
MOLATERS <molaters@outlook.com>
2023-07-27 15:08:25 +0800

    假数据实现

--- router/router.go
+++ router/router.go
@@ -13,5 +13,4 @@ func Router() {
 	http.HandleFunc("/", views.HTML.Index)
 	http.HandleFunc("/api/v1/post", api.API.SaveAndUpdatePost)
 	http.Handle("/resource/", http.StripPrefix("/resource/", http.FileServer(http.Dir("public/resource/"))))
-
 }
