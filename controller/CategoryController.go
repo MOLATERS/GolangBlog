6a0dfe0affd805fff307f8dec46d656dded21eb7
MOLATERS <molaters@outlook.com>
2023-08-02 14:55:40 +0800

    修改了数据库的问题

--- controller/CategoryController.go
+++ controller/CategoryController.go
@@ -34,7 +34,9 @@ func SearchCategoryName(c *gin.Context) {
 	var category model.Category
 	// 获取path中的分类id
 	categoryId := c.Params.ByName("id")
-	if err := db.Where("id = ?", categoryId).Find(&category).Error; err != nil {
+	sql := "SELECT * FROM categories WHERE id = ?"
+	//db.Where("id = ?", categoryId).First(&category).Error
+	if err := db.Raw(sql, categoryId).First(&category).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"code": 500,
 			"data": "",
@@ -42,6 +44,14 @@ func SearchCategoryName(c *gin.Context) {
 		})
 		return
 	}
+	//if err := db.Take(&category,model.Category{ID: (unint)(categoryId)},nil).Error; err{
+	//		c.JSON(http.StatusInternalServerError, gin.H{
+	//			"code": 500,
+	//			"data": "",
+	//			"msg":  "查找失败",
+	//		})
+	//		return
+	//}
 	c.JSON(http.StatusOK, gin.H{
 		"code": 200,
 		"data": gin.H{
--- model/category.go
+++ model/category.go
@@ -2,5 +2,9 @@ package model
 
 type Category struct {
 	ID           uint   `json:"id" gorm:"type:char(36);primary_key;"`
-	CategoryName string `json:"name" gorm:"type:varchar(50);not null"`
+	CategoryName string `json:"name" gorm:"type:varchar(50);not null;"`
+}
+
+func (c Category) TableName() string {
+	return "categories"
 }
